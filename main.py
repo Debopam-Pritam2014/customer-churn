964dc2b0026f178a271d00a052b0c1623343bc7f
Debopam Dey <letsdecode2014@gmail.com>
2024-10-11 15:19:37 +0530

    demo code add for prediction pipeline

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ def invoke_training_pipeline():
     
     except Exception as e:
         raise  CustomException(e,sys)
+def invoke_testing_pipeline():
+    try:
+        pass
+        # testing_pipeline=TrainingPipeline()
+        # testing_pipeline.initiate_training_pipeline()
+    
+    except Exception as e:
+        raise  CustomException(e,sys)
 
 if __name__=="__main__":
     invoke_training_pipeline()
